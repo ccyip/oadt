8883c0e8af6777e3d9c00328a3b75b22a0f2a1f4
Qianchuan Ye <yeqianchuan@gmail.com>
2021-02-16 17:58:10 -0500

    Add lemmas for [FinMap].

--- theories/base.v
+++ theories/base.v
@@ -4,9 +4,15 @@ From Hammer Require Export Tactics.
 
 (** * Lemmas *)
 
-Lemma insert_fresh_subseteq `{FinMapDom K M D} {A} : forall k (m : M A) v,
+Lemma insert_fresh_subseteq `{FinMapDom K M D} {A} k (m : M A) v :
   k ∉ dom D m ->
   m ⊆ <[k:=v]>m.
 Proof.
   intros. apply insert_subseteq. apply not_elem_of_dom. auto.
+Qed.
+
+Lemma map_empty_subseteq `{FinMap K M} {A} (m : M A) :
+  ∅ ⊆ m.
+Proof.
+  rewrite map_subseteq_spec. intros ??. by rewrite lookup_empty.
 Qed.
