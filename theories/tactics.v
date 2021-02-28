cd2138432b0fd5ac9e26a8a63bbb181e3128bdf6
Qianchuan Ye <yeqianchuan@gmail.com>
2021-02-28 17:50:58 -0500

    Add pruning process for set solving, which dramatically improve performance.

--- theories/tactics.v
+++ theories/tactics.v
@@ -92,3 +92,110 @@ Tactic Notation "fast_set_solver" :=
 slower. *)
 Tactic Notation "fast_set_solver" "*" :=
   try fast_done; set_unfold; qauto.
+
+Ltac set_fold_not :=
+  change (?x ∈ ?v -> False) with (x ∉ v) in *;
+  change (?x = ?v -> False) with (x <> v) in *.
+
+(** Pruning the hypotheses before set solving can *dramatically* improve
+performance. The following pruning tactics are based on heuristics, and they can
+make the goal unprovable. While they are unsound, they still work fine in our
+cases. A better approach is probably similar to finding a transitive closure of
+hypotheses against some "potentially needed" criteria. *)
+
+(** This pruning tactic is conservative, hence the name "safe". However, it can
+still make the goal unprovable. For example, it does not consider, say, that the
+set relations may appear in a conjunction. *)
+(* TODO: is it a way to check the conclusion of a hypothesis has certain "head"?
+I am using [context [_ -> _]] for now, which has some false negatives. *)
+Ltac set_prune_hyps_safe :=
+  simpl;
+  set_fold_not;
+  repeat
+    match goal with
+    | H : ?T |- _ =>
+      lazymatch T with
+      | _ ⊆ _ => fail
+      | _ ≡ _ => fail
+      | _ ∈ _ => fail
+      | _ ∉ _ => fail
+      | _ <> _ => fail
+      | context [_ -> _ ⊆ _] => fail
+      | context [_ -> _ ≡ _] => fail
+      | context [_ -> _ ∈ _] => fail
+      | context [_ -> _ ∉ _] => fail
+      | context [_ -> _ <> _] => fail
+      | _ => clear H
+      end
+    end.
+
+(** [set_hyp_filter] filters a hypothesis in continuation style, so we can
+thread a few filters. *)
+Tactic Notation "set_hyp_filter" constr(T) ">>=" tactic3(tac) :=
+  lazymatch T with
+  | _ ⊆ _ => fail
+  | _ ≡ _ => fail
+  | context [_ -> _ ⊆ _] => fail
+  | context [_ -> _ ≡ _] => fail
+  | _ => tac T
+  end.
+
+Tactic Notation "set_hyp_filter" constr(T) constr(x) ">>=" tactic3(tac) :=
+  lazymatch T with
+  | context [x] =>
+    lazymatch T with
+    | _ ∈ _ => fail
+    | _ ∉ _ => fail
+    | _ <> _ => fail
+    | context [_ -> _ ∈ _] => fail
+    | context [_ -> _ ∉ _] => fail
+    | context [_ -> _ <> _] => fail
+    | _ => tac T
+    end
+  | _ => tac T
+  end.
+
+(** This pruning tactic is more radical. It is more likely to destroy the
+goal, but it also offers better performance in certain cases. *)
+Ltac set_prune_hyps :=
+  simpl;
+  set_fold_not;
+  try
+    lazymatch goal with
+    | |- _ ⊆ _ =>
+      repeat
+        match goal with
+        | H : ?T |- _ =>
+          set_hyp_filter T >>= (fun _ => clear H)
+        end
+    | |- ?y ∉ _ =>
+      repeat
+        match goal with
+        | H : ?T |- _ =>
+          set_hyp_filter T >>= (fun T =>
+            set_hyp_filter T y >>= (fun _ => clear H))
+        end
+    | |- ?x <> ?y =>
+      repeat
+        match goal with
+        | H : ?T |- _ =>
+          set_hyp_filter T >>= (fun T =>
+            set_hyp_filter T x >>= (fun T =>
+              set_hyp_filter T y >>= (fun _ => clear H)))
+        end
+    end.
+
+Tactic Notation "set_solver" "!" :=
+  set_prune_hyps_safe; set_solver.
+Tactic Notation "set_solver" "!!" :=
+  set_prune_hyps; set_solver.
+
+Tactic Notation "fast_set_solver" "!" :=
+  set_prune_hyps_safe; fast_set_solver.
+Tactic Notation "fast_set_solver" "!!" :=
+  set_prune_hyps; fast_set_solver.
+
+Tactic Notation "fast_set_solver" "*" "!" :=
+  set_prune_hyps_safe; fast_set_solver*.
+Tactic Notation "fast_set_solver" "*" "!!" :=
+  set_prune_hyps; fast_set_solver*.
