f6440524ca3700217dc0495ed35937e5f7030455
Qianchuan Ye <yeqianchuan@gmail.com>
2021-08-13 19:26:59 -0400

    Build documentation.

---
+++ doc/custom.js
@@ -0,0 +1,17 @@
+var coqdocjs_conf = {
+    project_page: "https://github.com/ccyip/oadt",
+    repl: {
+        "forall": "∀",
+        "exists": "∃",
+        "~": "¬",
+        "/\\": "∧",
+        "\\/": "∨",
+        "<>": "≠",
+        "nat": "ℕ",
+        "bool": "𝔹"
+    },
+    more_repl: {
+        "fun" : "λ",
+    },
+    replInText: []
+}
