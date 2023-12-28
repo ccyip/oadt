383a0447796ff33c48420b313755810cb5c97d05
Qianchuan Ye <yeqianchuan@gmail.com>
2023-12-28 15:08:05 -0500

    Print out axioms

--- theories/lang_taypsi/metatheories.v
+++ theories/lang_taypsi/metatheories.v
@@ -104,3 +104,6 @@ Corollary obliviousness_open_obliv_val Σ x τ' e τ v1 v2 :
 Proof.
   eauto using obliviousness_open, indistinguishable_obliv_val, gdefs_typing_wf.
 Qed.
+
+Print Assumptions soundness.
+Print Assumptions obliviousness.
